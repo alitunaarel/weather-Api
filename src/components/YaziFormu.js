681bdb95c0ca5e6faa61498bfd926300ec599b32
ali <ali@iMac-de-Elodie.fritz.box>
2020-05-16 23:48:38 +0200

    features routers

--- src/components/YaziFormu.js
+++ src/components/YaziFormu.js
@@ -1,55 +1,67 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { withRouter } from "react-router-dom";
 
-
-const YaziFormu = () => {
+const YaziFormu = props => {
   const [yazi, setYazi] = useState({ title: "", content: "" });
+  const [hata, setHata] = useState("");
 
   const onInputChange = event =>
     setYazi({ ...yazi, [event.target.name]: event.target.value });
 
-    const onFromSubmit = event => {
-        event.preventDefault();
-        axios.post('https://react-yazi-yorum.herokuapp.com/posts',yazi)
-        .then(response => {
-            console.log(response)
-        })
-
-    } 
+  const onFromSubmit = event => {
+    event.preventDefault();
+    setHata("");
+    axios
+      .post("https://react-yazi-yorum.herokuapp.com/posts", yazi)
+      .then(response => {
+        props.history.push("/");
+      })
+      .catch(error => {
+        setHata("baslik ve yazi icerigi alanalri zorunludur..");
+      });
+  };
 
   return (
-    <div className="ui form">
-      <div className="field">
-        <label>Yazi Basligi</label>
-        <input
-          onChange={onInputChange}
-          value={yazi.title}
-          type="text"
-          name="title"
-        />
-      </div>
-      <div className="field">
-        <label>Yazi icerigi </label>
-        <textarea
-          onChange={onInputChange}
-          value={yazi.content}
-          rows="2"
-          name="content"
-        ></textarea>
-      </div>
-      <button className="ui primary button" onClick={onFromSubmit}>
+    <React.Fragment>
+      {hata && (
+        <div className="ui error message">
+          <div className="header">Hata </div>
+          <p>{hata}</p>
+        </div>
+      )}
+
+      <div className="ui form">
+        <div className="field">
+          <label>Yazi Basligi</label>
+          <input
+            onChange={onInputChange}
+            value={yazi.title}
+            type="text"
+            name="title"
+          />
+        </div>
+        <div className="field">
+          <label>Yazi icerigi </label>
+          <textarea
+            onChange={onInputChange}
+            value={yazi.content}
+            rows="2"
+            name="content"
+          ></textarea>
+        </div>
+        <button className="ui primary button" onClick={onFromSubmit}>
           Gonder
-          </button>
-      <button className="ui button">Iptal Et</button>
-      
-    </div>
+        </button>
+        <button className="ui button">Iptal Et</button>
+      </div>
+    </React.Fragment>
   );
 };
 
-export default YaziFormu;
+export default withRouter(YaziFormu);
 
-
-// Degerli Hocam, video herzaman ki gibi harika olmus emeginize saglik, 
+// Degerli Hocam, video herzaman ki gibi harika olmus emeginize saglik,
 // Sesli dusundugunuz kisimalri birakmaniz cok yararli oluyor kanaatimce,
-//  tesekkur ederim. Musadenizle bir soru sormak istiyorum,React redux kullanip bir state 
+//  tesekkur ederim. Musadenizle bir soru sormak istiyorum,React redux kullanip bir state
 //  manegement
